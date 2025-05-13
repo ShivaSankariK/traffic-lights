34465e2a99882c680caa07599ff29fd004c396d7
ShivaSankariK <shivasankari.k1401@gmail.com>
2025-05-13 14:42:46 +0530

    Added changes

--- traffic-lights-app/src/App.js
+++ traffic-lights-app/src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Traffic signal</h1>
       <div className="wrapper">
         <TrafficLights />
+        <div className='traffic-stand'/>
       </div>
     </div>
   );
